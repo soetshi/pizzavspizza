5022888aaab9e2ad97e6c69172d93afaf22c9bb7
soetshi <90394673+soetshi@users.noreply.github.com>
2023-02-25 14:41:34 +0100

    Initial commit

---
+++ pizzaproject/stores/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PizzeriaListSerializer
+from .serializers import PizzeriaDetailSerializer
+from .models import Pizzeria
+
+# Create your views here.
+
+class PizzeriaListAPIView(generics.ListAPIView):
+
+    queryset= Pizzeria.objects.all()
+    serializer_class = PizzeriaListSerializer
+
+
+
+class PizzeriaRetrieveAPIView(generics.RetrieveAPIView):
+    lookup_field="id"
+    queryset= Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
+
+
+class PizzeriaCreateAPIView(generics.CreateAPIView):
+    
+    queryset= Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
+
+
+class PizzeriaRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    lookup_field="id"
+    queryset= Pizzeria.objects.all()
+    serializer_class = PizzeriaDetailSerializer
+
+
+class PizzeriaDestroyAPIView(generics.DestroyAPIView):
+    lookup_field="id"
+    queryset= Pizzeria.objects.all()
+    
+
+
+
